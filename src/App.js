4afb2e52df492ad93f81f69d37ebffe9e10b0c8b
AsrithaCherukuru <cherukurusaisriasritha@gmail.com>
2023-10-22 23:47:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import insta from './instagramlogo.png';
+import mail from './gmail.jpeg';
+import github from './githublogo.png';
+export default function app(){
+  return(
+    <div id="id">
+      <div id="id1">
+        <p>My Own Portfolio</p>
+      </div>
+      <div id="id2">
+        <p><b id="id3">Profile</b></p>
+        <p>Hello I am Cherukuru SaisriAsritha currently pursing in BTech-3rd year in  VIT-AP university.
+        I completed my Intermediate at Narayana</p><p>junior College and my SSC at Sri Chaitanya E.M School in Kavali.
+             I like to spend my time workingon exciting projects.I am passionate </p><p>Full Stack Developer with expertise in javascript ,ReactJs,NodeJs.
+          I enjoy creating web applications and solving complex problems.</p>
+      </div>
+
+      <div id="id9">
+        <p id="id3"><b>Skills</b></p>
+        
+        <span id="id6">Java</span>
+        <span id="id6">Python</span>
+        <span id="id6">DSA</span>
+        <span id="id6">HTML & CSS</span>
+        <span id="id6">SQL</span>
+        <span id="id6">JavaScript</span>
+      </div>
+      <div id="id10">
+        <p id="id12"><b>Projects</b></p>
+        
+      <span id="id11">Weather Monitoring System</span>
+        <span id="id11">Driver Drowsiness Detection</span>
+        <span id="id11">Air Quality Displayer</span>
+      </div>
+      <div id="id4">
+        <p id="id3">Contact</p>
+        <span id="id5"><a id="id8" href="cherukurusaisriasritha@gmail.com">Email</a></span>
+        <a href="cherukurusaisriasritha@gmail.com"><img src={mail} alt="Instagram logo" height={50}/></a>
+        <span id="id5"><a id="id8" href="https://www.instagram.com/asritha.cherukuru/">Instagram</a></span>
+        <a href="https://www.instagram.com/asritha.cherukuru/"><img src={insta} alt="Instagram logo" height={50}/></a>
+        <span id="id5"><a id="id8" href="https://github.com/AsrithaCherukuru ">Github</a></span>
+        <a href="https://github.com/AsrithaCherukuru"><img src={github} alt="Instagram logo" height={50}/></a>
+      </div>
+    </div>
+  );
+}
+// import './App.css';
+// import insta from './instagramlogo.png';
+// import mail from './gmail.jpeg';
+// import github from './githublogo.png';
+// export default function app(){
+//   return(
+//     <div id="id">
+//       <div id="id1">
+//         <p>My Portfolio</p>
+//       </div>
+//       <div id="id2">
+//         <p><b id="id3">Profile</b></p>
+//         
+//       </div>
+      
+//        <div id="id9">
+//         <p id="id3"><b>Skills</b></p> 
+//         <span id="id6">Java</span>
+//         <span id="id6">Python</span>
+//         <span id="id6">DSA</span>
+//         <span id="id6">HTML & CSS</span>
+//         <span id="id6">SQL</span>
+//         <span id="id6">JavaScript</span>
+//       </div>
+//       <div id="id10">
+//         <p id="id12"><b>Projects</b></p>
+        
+//         <span id="id11">Weather Monitoring System</span>
+//         <span id="id11">Driver Drowsiness Detection</span>
+//         <span id="id11">Air Quality Displayer</span>
+//       </div>
+//       <div id="id4">
+//         <p id="id3">Contact</p>
+//         <span id="id5"><a id="id8" href="cherukurusaisriasritha@gmail.com">Email</a></span>
+//         <a href="cherukurusaisriasritha@gmail.com"><img src={mail} height={30}/></a>
+//         <span id="id5"><a id="id8" href="https://www.instagram.com/asritha.cherukuru/">Instagram</a></span>
+//         <a href="https://www.instagram.com/asritha.cherukuru/"><img src={insta} height={30}/></a>
+//         <span id="id5"><a id="id8" href="https://github.com/AsrithaCherukuru ">Github</a></span>
+//         <a href="https://github.com/AsrithaCherukuru "><img src={github}height={30}/></a>
+
+//       </div>
+//     </div>
+//   );
+// }
